01878a856396d0b2d040888163e1f567c958f04d
Firas <mehdihannachi02@gmail.com>
2021-12-21 10:59:37 +0100

    check if user is authenticated using passport jwt strategy

---
+++ middlewares/passport-setup.js
@@ -0,0 +1,19 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.secretOrKey,
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    const user = await User.findById(jwt_payload.id);
+
+    user ? done(false, user) : done(null, false);
+  })
+);
+
+module.exports = isAuth = () =>
+  passport.authenticate("jwt", { session: false });
